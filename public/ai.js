499df1a6b725e95d9a97e120a833f99e0bcc9678
Miklos Lukasik <szarhazi@gmail.com>
2015-06-05 04:27:57 +0100

    ai.js

---
+++ public/ai.js
@@ -0,0 +1,13 @@
+function ai(tablE,wn){
+	tableToAi=createFirstTableState(tablE,wn)
+
+	// tableToAi.forEach(function(tableTo){
+	// 	console.log(tableTo[0],tableTo[1],tableTo[2],tableTo[3])
+	// })
+	//aiLoop([table],1,wn,wn)
+	// console.clear()
+	// for (var i=0;i<tableToAi.length;i++)
+	// 	console.log(tableToAi[i])
+	return tableToAi[1][0]
+
+}
