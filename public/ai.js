c9fa82ba31695db834976d0f0b59ef04cb8f3d4b
Miklos Lukasik <szarhazi@gmail.com>
2015-06-05 04:35:06 +0100

    ai in no ai

--- public/ai.js
+++ public/ai.js
@@ -1,3 +1,105 @@
+
+
+
+function createFirstTableState(cfTable,cfColor){
+	
+	var cfMoves=moveArrayToStrings(getAllMoves(getTableData(cfTable,cfColor),cfTable,cfColor))
+	// for(var i=cfMoves.length-1;i>=0;i--){
+	// 	if(validateTable(moveIt(cfMoves[i],table),!wNext)==9){
+	// 		cfMoves.splice(i,1)
+	// 	}
+	// }
+
+
+
+
+	var tempTable=new Array(8)
+	var allTempTables=[]
+	var opponentsOrigValue=validateTable(cfTable,!cfColor)
+	var myOrigValue=validateTable(cfTable,cfColor)
+
+	allTempTables.push([cfColor,fadeConst,0])				//array heading:color,fadeConst(will be multiplied),howDeep
+	
+	i=0
+	
+	cfMoves.forEach(function(stepMove){
+		
+		tempTable=moveIt(stepMove,cfTable)
+		
+		var myOrigValue=hitValue
+		hitValue=0
+	
+		tTableValue=myOrigValue-escConst*(validateTable(tempTable,!cfColor)-opponentsOrigValue)+(validateTable(tempTable,cfColor)/2)
+		//console.log(tTableValue)
+		// allTempTables.push([stepMove,tTableValue])
+
+		//tempTableValue=0
+		
+
+		// one deeper
+
+		var cf2Moves=moveArrayToStrings(getAllMoves(getTableData(tempTable,cfColor),tempTable,cfColor))
+		//console.log(cf2Moves)
+		var tempTable2=new Array(8)
+		var tTable2Value=0
+		//var allTempTables=[]
+		var opponents2OrigValue=validateTable(tempTable,!cfColor)
+		var myOrigValue=validateTable(tempTable,cfColor)
+
+		cf2Moves.forEach(function(step2Move,moveNo){
+
+			temp2Table=moveIt(step2Move,tempTable)
+
+			var myOrig2Value=hitValue
+			hitValue=0
+			
+			tTable2Value -=myOrig2Value-(escConst*escConst*(validateTable(temp2Table,!cfColor)-
+				opponents2OrigValue)+(validateTable(temp2Table,cfColor)/10)/10)
+	
+			//alert(tTable2Value)
+	
+		})
+
+
+
+		// deepening ends
+
+
+
+
+		//allTempTables.push([stepMove,tTableValue])
+
+
+
+
+
+
+
+
+
+		//totalArrayValue=0
+
+		//aiLoop([tempTable],!cfColor,cfColor,myOrigValue)
+
+		var repValue=tTable2Value/50+tTableValue
+		allTempTables.push([stepMove,
+		parseInt(repValue*100),parseInt(tTableValue*100),parseInt(tTable2Value*100)])//,tableHitValue]) //row: movestring, ai val, deepen val, deep hit val, [tables]
+		
+	})
+	//console.clear()
+	tempstr=""
+	if(wNext){
+		tempstr="White's move, "
+	}else{
+		tempstr="Black's move, "
+	}	
+	//console.log(tempstr+i+' tables generated.')
+	allTempTables=allTempTables.sort(sortAiArray)
+	return allTempTables
+}
+
+
+
 function ai(tablE,wn){
 	tableToAi=createFirstTableState(tablE,wn)
 
