599afaa4130c20c05544d6d10a750b509f8f4e5d
mikilukasik <szarhazi@gmail.com>
2015-06-10 23:57:08 +0100

    .

--- server.js
+++ server.js
@@ -104,9 +104,13 @@ setInterval(function(){
 					
 					
 						initTable(xx)
-						lobbyChat[xx]=chatTemp
-						lobbyChat[xx].push(t1const)
-						lobbyChat[xx].push(allWNexts[xx])
+						allChats[xx]=chatTemp
+						lobbyChat.push(' ')
+						lobbyChat.push('Table: '+xx)
+						
+						
+						lobbyChat.push('t1const: '+t1const)
+						lobbyChat.push('wStuck: 'allWNexts[xx])
 						//aiOn[xx]=true
 						randomConst=Math.random()*20
 						console.log('aimove on table '+xx+' reset.')
